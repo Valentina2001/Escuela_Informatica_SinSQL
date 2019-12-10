3671855dfb2f3468ccb7ea15a1f7a7c7d2aacdc7
Valentina <valen.caldera17@gmail.com>
2019-12-10 16:17:10 -0500

    Form alumno funcionando

--- Ejercicio2SinSQL/Alumno.cs
+++ Ejercicio2SinSQL/Alumno.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace Ejercicio2SinSQL
 {
     public class Alumno
     {
+
+        [Key]
         public int Num_Matricula { get; set; }
         public int Dni { get; set; }
         public string Nombre { get; set; }
